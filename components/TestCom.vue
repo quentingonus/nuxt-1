8bb74932ece59f07bec02587532f0dcb693bd2d4
quentingonus <55360738+quentingonus@users.noreply.github.com>
2020-11-04 01:00:29 +0630

    started

---
+++ components/TestCom.vue
@@ -0,0 +1,6 @@
+<template>
+<h1>Tmp Component</h1>
+</template>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <TestCom />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style>
+</style>
